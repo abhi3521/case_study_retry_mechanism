3afd7e19517eec2138d897b998afec4201481313
abhi3521 <abhiaryan3521@gmail.com>
2026-05-25 10:11:35 +0530

    Start

---
+++ src/main/java/com/case_study/retry_case/RetryCaseApplication.java
@@ -0,0 +1,13 @@
+package com.case_study.retry_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetryCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/case_study/retry_case/RetryCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.case_study.retry_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetryCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
